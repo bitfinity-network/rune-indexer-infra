f5bb895481484359db85dc890933169f4ba775e4
Yasir <yasir@shariff.dev>
2024-09-25 12:17:35 +0300

    Refactor Terraform configuration: remove unused files and simplify startup script for Bitcoin Core and ord services

--- main.tf
+++ main.tf
@@ -27,41 +27,12 @@ resource "google_compute_instance" "rune_indexer" {
     email  = data.google_compute_default_service_account.default_sa.email
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo mkdir -p /opt/indexer",
-      "sudo chown -R indexer:indexer /opt/indexer",
-      "sudo chmod -R 700 /opt/indexer"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "indexer"
-    private_key = tls_private_key.blockscout_key.private_key_pem
-    host = self.network_interface[0].access_config[0].nat_ip
-    agent = false
-  }
-
-  provisioner "file" {
-    content     = local.bitcoin_conf
-    destination = "/opt/indexer/bitcoin.conf"
-  }
-
-  provisioner "file" {
-    content     = local.ord_config
-    destination = "/opt/indexer/ord.yaml"
-  }
-
   allow_stopping_for_update = true
 
   metadata_startup_script = local.startup_script
 
   metadata = {
     container-config-hash = sha256(local.startup_script)
-    ssh-keys = "indexer:${tls_private_key.blockscout_key.public_key_openssh}"
   }
 
   tags = ["rune-indexer", var.network]
@@ -75,21 +46,4 @@ locals {
       network              = var.network
       index_start_height   = var.index_start_height
     })
-
-    ord_config = templatefile("${path.module}/templates/ord.yaml.tftpl", {
-      bitcoin_rpc_username = var.bitcoin_rpc_user
-      bitcoin_rpc_password = var.bitcoin_rpc_password
-      network              = var.network
-      index_start_height   = var.index_start_height
-    })
-
-    bitcoin_conf = templatefile("${path.module}/templates/bitcoin.conf.tftpl", {
-      bitcoin_rpc_user     = var.bitcoin_rpc_user
-      bitcoin_rpc_password = var.bitcoin_rpc_password
-      network              = var.network
-    })
-}
-
-resource "tls_private_key" "blockscout_key" {
-  algorithm = "RSA"
 }
